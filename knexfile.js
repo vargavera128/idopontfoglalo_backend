46c9edf40c8273edd587fbe4232797db636d3582
Varga Veronika <varga.veronika@hallgato.sze.hu>
2024-09-06 19:29:42 +0200

    UPDATE - fix

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,22 @@
-// Update with your config settings.
+const databaseConfig = {
+  client: "postgresql",
+  connection: {
+    host: process.env.DB_HOST || "localhost", //update to match the service name in Docker Compose
+    database: process.env.DB_NAME || "idopontfoglalo",
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "password",
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+  },
+};
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+  development: databaseConfig,
+  staging: databaseConfig,
+  production: databaseConfig,
 };
